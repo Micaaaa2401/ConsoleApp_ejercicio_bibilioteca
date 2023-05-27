6a399708efad78eabf3cd34154db38467501fc4f
Micaaaa2401 <micaort@scanntech.com>
2023-05-27 16:27:01 -0300

    correccion de clase libro

--- ConsoleApp_ejercicio_bibilioteca/Libro.cs
+++ ConsoleApp_ejercicio_bibilioteca/Libro.cs
@@ -13,6 +13,9 @@ namespace ConsoleApp_ejercicio_bibilioteca
         private string _autor;
         private DateTime _fechaPublicacion;
         private string _editorial;
+        public int _edicion;
+        public int _paginas;
+        public string _tema;
         private List<Ejemplar> _ejemplares;
 
         public int IdLibro
@@ -45,14 +48,32 @@ namespace ConsoleApp_ejercicio_bibilioteca
             set { _editorial = value; }
         }
 
+        public int Paginas
+        {
+            get { return _paginas;}
+            set { _paginas = value; }
+        }
+
+        public int Edicion
+        {
+            get { return _edicion;}
+            set { _edicion = value; }
+        }
+
+        public string Tema
+        {
+            get { return _tema; }
+            set { _tema = value; }
+        }
         public List<Ejemplar> Ejemplares
         {
             get { return _ejemplares; }
             set { _ejemplares= value; }
         }
 
+
         public Libro(int idLibro, string titulo, string autor, DateTime fechaPublicacion,
-            string editorial)
+            string editorial, int paginas, int edicion, string tema)
         {
             idLibro = IdLibro;
             Titulo = titulo;
@@ -60,7 +81,13 @@ namespace ConsoleApp_ejercicio_bibilioteca
             FechaPublicacion = fechaPublicacion;
             Editorial = editorial;
             Ejemplares = new List<Ejemplar>();
+            Tema = tema;
+            Edicion = edicion;
+            Paginas = paginas;
 
         }
+
+
+        //pendiente hacer los metodos
     }
 }
